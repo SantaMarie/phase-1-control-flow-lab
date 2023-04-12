dcab5a91d54d2c69239e7b249bc5f7a3966ec050
SantaMarie <59677546+SantaMarie@users.noreply.github.com>
2023-04-12 16:37:25 -0700

    adding indext.js to file

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+if(distance <= 400){
+    return 'This one is on me!';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (distance > 400 && distance < 2000){
+    return 'That will be twenty bucks.';
+}
+else if (distance > 2000 && distance < 2500 ){
+    return 'I will gladly take your thirty bucks.';
+}
+else if (distance >= 2500){
+    return 'No can do.';
+
+}
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let response = city === `NYC` ? `Ok, sounds good.` : `No go.`;
+  return response
 }
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip){
+    case "generous":
+    return("Thank you so much.");
+      case "not as generous":
+        return ("Thank you.");
+         case "thanks for everything":
+          return ("Bye.");
+          
+} 
+  } 
